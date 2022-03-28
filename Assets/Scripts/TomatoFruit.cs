dcd5548100f675cd180b2a4797bee5e4432bbeac
ColinSYang <70976943+ColinSYang@users.noreply.github.com>
2022-03-28 16:02:29 -0400

    Add new fruits and locations to the date selection screen

--- Assets/Scripts/TomatoFruit.cs
+++ Assets/Scripts/TomatoFruit.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class BlueberryFruit : Fruit
+public class TomatoFruit : Fruit
 {
     private static int numDates = 0;
     public static int daysLeftUntilDateable = 0;
 
-    private List<KitchenLocation> dateLocations = new List<KitchenLocation> { KitchenLocation.Blender, KitchenLocation.OverheadLamp };
+    private List<KitchenLocation> dateLocations = new List<KitchenLocation> { KitchenLocation.LeftTable, KitchenLocation.LowerCabinet };
 
     public override void InitiateDate()
     {
         numDates++;
         daysLeftUntilDateable = 2;
-        SceneManager.LoadScene("Blueberry Date " + numDates);
+        SceneManager.LoadScene("Tomato Date " + numDates);
     }
 
     public override int SetLocation()
