a7377636d39f35747e77afe56117f23d77ac771e
zcolucci1012 <32983507+zcolucci1012@users.noreply.github.com>
2022-04-04 16:35:41 -0400

    Added Jar
    - Needs more balancing work

--- Assets/Scripts/Combat Scripts/FryingPan.cs
+++ Assets/Scripts/Combat Scripts/FryingPan.cs
@@ -25,12 +25,13 @@ public class FryingPan : Fighter
             if (healing == 0) {
                 msg += "It can't heal!";
             } else {
-                msg += "It healed itself for " + healing + " HP";
+                msg += "It healed its partner for " + healing + " HP";
             }
             return msg;
         };
 
         this.attack1 = new Attack(this.attack1name, this.attack1desc, this.attack1type, this.attack1execute);
         this.attack2 = new Attack(this.attack2name, this.attack2desc, this.attack2type, this.attack2execute);
+        this.attack3 = new Attack(this.attack3name, this.attack3desc, this.attack3type, this.attack3execute);
     }
 }
