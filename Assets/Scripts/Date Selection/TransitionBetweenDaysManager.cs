528c9ea43fa418671e9bf2a477f58746942df7b6
ColinSYang <70976943+ColinSYang@users.noreply.github.com>
2022-04-04 16:36:11 -0400

    Changing sprites based on relative relationship change

--- Assets/Scripts/Date Selection/TransitionBetweenDaysManager.cs
+++ Assets/Scripts/Date Selection/TransitionBetweenDaysManager.cs
@@ -6,6 +6,14 @@ using UnityEngine.SceneManagement;
 
 public class TransitionBetweenDaysManager : MonoBehaviour
 {
+    public Image strawberryFruitImage;
+    public Image blueberryFruitImage;
+    public Image lemonFruitImage;
+    public Image blackberryFruitImage;
+    public Image tomatoFruitImage;
+
+    public List<Sprite> emotionSprites;
+
     public Image strawberryRelativeRelationshipImage;
     public Image blueberryRelativeRelationshipImage;
     public Image lemonRelativeRelationshipImage;
@@ -44,11 +52,11 @@ public class TransitionBetweenDaysManager : MonoBehaviour
 
     private void SetRelativeRelationshipImages()
     {
-        SetRelationshipImage(RelationshipScore.strawberryScore - strawberryPreviousRelationshipScore, strawberryRelativeRelationshipImage, strawberryRelationshipText, "Strawberry");
-        SetRelationshipImage(RelationshipScore.blueberryScore - blueberryPreviousRelationshipScore, blueberryRelativeRelationshipImage, blueberryRelationshipText, "Blueberry");
-        SetRelationshipImage(RelationshipScore.lemonScore - lemonPreviousRelationshipScore, lemonRelativeRelationshipImage, lemonRelationshipText, "Lemon");
-        SetRelationshipImage(RelationshipScore.blackberryScore - blackberryPreviousRelationshipScore, blackberryRelativeRelationshipImage, blackberryRelationshipText, "Blackberry");
-        SetRelationshipImage(RelationshipScore.tomatoScore - tomatoPreviousRelationshipScore, tomatoRelativeRelationshipImage, tomatoRelationshipText, "Tomato");
+        SetRelationshipImage(RelationshipScore.strawberryScore - strawberryPreviousRelationshipScore, strawberryRelativeRelationshipImage, strawberryFruitImage, strawberryRelationshipText, "Strawberry", 0);
+        SetRelationshipImage(RelationshipScore.blueberryScore - blueberryPreviousRelationshipScore, blueberryRelativeRelationshipImage, blueberryFruitImage, blueberryRelationshipText, "Blueberry", 1);
+        SetRelationshipImage(RelationshipScore.lemonScore - lemonPreviousRelationshipScore, lemonRelativeRelationshipImage, lemonFruitImage, lemonRelationshipText, "Lemon", 2);
+        SetRelationshipImage(RelationshipScore.blackberryScore - blackberryPreviousRelationshipScore, blackberryRelativeRelationshipImage, blackberryFruitImage, blackberryRelationshipText, "Blackberry", 3);
+        SetRelationshipImage(RelationshipScore.tomatoScore - tomatoPreviousRelationshipScore, tomatoRelativeRelationshipImage, tomatoFruitImage, tomatoRelationshipText, "Tomato", 4);
 
         strawberryPreviousRelationshipScore = RelationshipScore.strawberryScore;
         blueberryPreviousRelationshipScore = RelationshipScore.blueberryScore;
@@ -57,30 +65,35 @@ public class TransitionBetweenDaysManager : MonoBehaviour
         tomatoPreviousRelationshipScore = RelationshipScore.tomatoScore;
     }
 
-    private void SetRelationshipImage(int delta, Image image, Text text, string name)
+    private void SetRelationshipImage(int delta, Image image, Image fruitImage, Text text, string name, int index)
     {
         if (delta < -2)
         {
+            fruitImage.sprite = emotionSprites[index*3 + 2];
             image.sprite = doubleDownArrow;
             text.text = name + " had an awful time with you. You said some things that they really didn't want to hear.";
         }
         else if (delta < 0)
         {
+            fruitImage.sprite = emotionSprites[index*3 + 2];
             image.sprite = singleDownArrow;
             text.text = name + " didn't have much fun with you. You should be more careful with what you say to them.";
         }
         else if (delta > 2)
         {
+            fruitImage.sprite = emotionSprites[index*3 + 1];
             image.sprite = doubleUpArrow;
             text.text = name + " had an amazing time with you. They can't wait to go on another date with you.";
         }
         else if (delta > 0)
         {
+            fruitImage.sprite = emotionSprites[index*3 + 1];
             image.sprite = singleUpArrow;
             text.text = name + " had some fun hanging out with you. They wouldn't mind going on another date with you.";
         }
         else
         {
+            fruitImage.sprite = emotionSprites[index*3 + 0];
             image.sprite = noChange;
             text.text = name + " did their own thing. Maybe you should take them out on a date to see what they're up to.";
         }
