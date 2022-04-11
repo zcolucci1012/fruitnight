2ca4c522e4fdbf0fd315dc4efeeeb67c4e9126ea
zcolucci1012 <32983507+zcolucci1012@users.noreply.github.com>
2022-04-11 16:43:13 -0400

    Finished up Lemon & Blackberry Combat
    - Lemon combat 2 complete
    - Blackberry combat 2 also complete

--- Assets/Scripts/Combat Scripts/Enemies/DustBunny.cs
+++ Assets/Scripts/Combat Scripts/Enemies/DustBunny.cs
@@ -35,6 +35,5 @@ public class DustBunny : Fighter
 
         this.attack1 = new Attack(this.attack1name, this.attack1desc, this.attack1type, this.attack1execute);
         this.attack2 = new Attack(this.attack2name, this.attack2desc, this.attack2type, this.attack2execute);
-        this.attack3 = new Attack(this.attack3name, this.attack3desc, this.attack3type, this.attack3execute);
     }
 }
